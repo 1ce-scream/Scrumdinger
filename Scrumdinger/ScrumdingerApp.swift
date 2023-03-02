fdea3cb61a475ac3531bfc053ba0b5a203e715aa
Vitaliy Viktorovich <po4tavela@gmail.com>
2023-03-02 16:22:32 +0500

    Init. Первые наброски интерфейса.

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Vitaliy Talalay on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}

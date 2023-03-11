5feff524f04b43c28e02bd0492c5a64282a71a31
Vitaliy Viktorovich <po4tavela@gmail.com>
2023-03-11 13:27:21 +0300

    Добавлено распознавание речи для записи в историю текста выступления спикеров

--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -12,16 +12,25 @@ struct History: Identifiable, Codable {
     let date: Date
     var attendees: [DailyScrum.Attendee]
     var lengthInMinutes: Int
+    var transcript: String?
     
     init(
         id: UUID = UUID(),
         date: Date = Date(),
         attendees: [DailyScrum.Attendee],
-        lengthInMinutes: Int = 5
+        lengthInMinutes: Int = 5,
+        transcript: String? = nil
     ) {
         self.id = id
         self.date = date
         self.attendees = attendees
         self.lengthInMinutes = lengthInMinutes
+        self.transcript = transcript
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
     }
 }
